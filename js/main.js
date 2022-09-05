c7aa051403b7dde8eb38fdde9a89d2f1902f1b9a
beaverbeg <beaverbeg@gmail.com>
2022-09-05 21:45:47 +0200

    new.html 1.4

--- js/main.js
+++ js/main.js
@@ -1,13 +1,35 @@
-var currentLink = "https://beaverbeg.xyz"
+var currentLink = "https://beaverbeg.xyz/";
+var currentFartbinLink = "https://fartbin.cf/";
+var currentWebpassLink = "https://pass.beaverbeg.xyz";
+var currentCubeLink = "https://cube.beaverbeg.xyz";
+var currentCodesLink = "https://codes.beaverbeg.xyz";
 
 console.log("page is loaded");
 
-function getCurrentLink(){
-    return currentLink;
+function addCurrentLink(link, id){
+    var linkToAdd;
+    if(link=="beaverbeg"){
+        linkToAdd = currentLink;
+    }
+    if(link=="fartbin"){
+        linkToAdd = currentFartbinLink;
+    }
+    if(link=="webpass"){
+        linkToAdd = currentWebpassLink;
+    }
+    if(link=="cube"){
+        linkToAdd = currentCubeLink;
+    }
+    if(link=="codes"){
+        linkToAdd = currentCodesLink;
+    }
+
+    document.getElementById(id).href = linkToAdd;
+
 }
 function NewTab(link){
     window.open(link+'','_blank')
 }
 function ThisTab(link){
-    window.open(link)
+    window.open(link+'', '_self')
 }
