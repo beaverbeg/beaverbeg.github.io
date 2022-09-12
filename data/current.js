02849740d0a605e1e27702795334f31df438636e
beaverbeg <81553355+beaverbeg@users.noreply.github.com>
2022-09-12 17:25:48 +0200

    Update current.js

--- data/current.js
+++ data/current.js
@@ -19,6 +19,14 @@ data = `
 "pass":[
     {"link": "https://pass.beaverbeg.xyz"},
     {"desc": "Simple password generator"}
+],
+"tb":[
+    {"link": "https://tb.beaverbeg.repl.co"},
+    {"desc": "Simple js canvas game"}
+],
+"rps":[
+    {"link": "https://rps.beaverbeg.repl.co"},
+    {"desc": "Rock paper scissors game"}
 ]
 }
 `;
