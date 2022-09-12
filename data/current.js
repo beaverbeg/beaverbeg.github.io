515efefc3aad46051926fca769463e7137a45cd4
beaverbeg <beaverbeg@gmail.com>
2022-09-12 17:10:08 +0200

    links update
    added universal source of my main links

---
+++ data/current.js
@@ -0,0 +1,24 @@
+data = `
+{
+"main":[
+    {"link": "https://beaverbeg.xyz"},
+    {"desc": "My official website"}
+],
+"fartbin":[
+    {"link": "https://fartbin.cf"},
+    {"desc": "Note saving application"}
+],
+"cube":[
+    {"link": "https://cube.beaverbeg.xyz"},
+    {"desc": "3D cube generator made in three.js"}
+],
+"codes":[
+    {"link": "https://codes.beaverbeg.xyz"},
+    {"desc": "Website for my projects"}
+],
+"pass":[
+    {"link": "https://pass.beaverbeg.xyz"},
+    {"desc": "Simple password generator"}
+]
+}
+`;
--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-var currentLink = "https://beaverbeg.xyz/";
-var currentFartbinLink = "https://fartbin.cf/";
-var currentWebpassLink = "https://pass.beaverbeg.xyz";
-var currentCubeLink = "https://cube.beaverbeg.xyz";
-var currentCodesLink = "https://codes.beaverbeg.xyz";
-
+var mydata = JSON.parse(data);
 console.log("page is loaded");
+
+var currentLink =        mydata.main[0].link;
+var currentFartbinLink = mydata.fartbin[0].link;
+var currentWebpassLink = mydata.pass[0].link;
+var currentCubeLink =    mydata.cube[0].link;
+var currentCodesLink =   mydata.codes[0].link;
+
+var currentFartbinDesc = mydata.fartbin[1].desc;
+var currentWebpassDesc = mydata.pass[1].desc;
+var currentCubeDesc =    mydata.cube[1].desc;
+document.getElementById("fartbin-desc").innerHTML = "- "+currentFartbinDesc;
+document.getElementById("cube-desc").innerHTML =    "- "+currentCubeDesc;
+document.getElementById("pass-desc").innerHTML =    "- "+currentWebpassDesc;
+
 
 function addCurrentLink(link, id){
     var linkToAdd;
@@ -25,8 +34,8 @@ function addCurrentLink(link, id){
     }
 
     document.getElementById(id).href = linkToAdd;
-
 }
+
 function NewTab(link){
     window.open(link+'','_blank')
 }
