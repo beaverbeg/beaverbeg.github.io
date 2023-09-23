631b95452ee72ee5aae536e56f6e0b901e3be25a
beaverbeg <81553355+beaverbeg@users.noreply.github.com>
2023-09-23 17:23:13 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,12 +8,14 @@ var cWebpassLink =   mydata.pass[0].link;
 var cCubeLink =      mydata.cube[0].link;
 var cTheboxLink =    mydata.tb[0].link;
 var cRockpaperLink = mydata.rps[0].link;
+var cSomegameLink = mydata.somegame[0].link;
 
 var cFartbinDesc =   mydata.fartbin[1].desc;
 var cWebpassDesc =   mydata.pass[1].desc;
 var cCubeDesc =      mydata.cube[1].desc;
 var cTheboxDesc =    mydata.tb[1].desc;
 var cRockpaperDesc = mydata.rps[1].desc;
+var cSomegameDesc = mydata.somegame[1].desc;
 
 try{
     document.getElementById("fartbin-desc").innerHTML = "- "+cFartbinDesc;
@@ -21,6 +23,7 @@ try{
     document.getElementById("cube-desc").innerHTML =    "- "+cCubeDesc;
     document.getElementById("tb-desc").innerHTML =      "- "+cTheboxDesc;
     document.getElementById("rps-desc").innerHTML =     "- "+cRockpaperDesc;
+    document.getElementById("somegame-desc").innerHTML =     "- "+cSomegameDesc;
 }
 catch(err){
     console.log("could not add description to projects\n");
@@ -46,6 +49,9 @@ function addCurrentLink(link, id){
     }
     if(link=="rps"){
         linkToAdd = cRockpaperLink;
+    }
+    if(link=="somegame"){
+        linkToAdd = cSomegameLink;
     }
 
     document.getElementById(id).href = linkToAdd;
