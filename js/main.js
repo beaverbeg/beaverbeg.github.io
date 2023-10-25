ccc419008fd0c8976aff495e923db9a3899ba9f3
beaverbeg <81553355+beaverbeg@users.noreply.github.com>
2023-10-25 22:19:21 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,14 +7,12 @@ var cFartbinLink =   mydata.fartbin[0].link;
 var cWebpassLink =   mydata.pass[0].link;
 var cCubeLink =      mydata.cube[0].link;
 var cTheboxLink =    mydata.tb[0].link;
-var cRockpaperLink = mydata.rps[0].link;
 var cSomegameLink = mydata.somegame[0].link;
 
 var cFartbinDesc =   mydata.fartbin[1].desc;
 var cWebpassDesc =   mydata.pass[1].desc;
 var cCubeDesc =      mydata.cube[1].desc;
 var cTheboxDesc =    mydata.tb[1].desc;
-var cRockpaperDesc = mydata.rps[1].desc;
 var cSomegameDesc = mydata.somegame[1].desc;
 
 try{
@@ -22,7 +20,6 @@ try{
     document.getElementById("pass-desc").innerHTML =    "- "+cWebpassDesc;
     document.getElementById("cube-desc").innerHTML =    "- "+cCubeDesc;
     document.getElementById("tb-desc").innerHTML =      "- "+cTheboxDesc;
-    document.getElementById("rps-desc").innerHTML =     "- "+cRockpaperDesc;
     document.getElementById("somegame-desc").innerHTML =     "- "+cSomegameDesc;
 }
 catch(err){
@@ -46,9 +43,6 @@ function addCurrentLink(link, id){
     }
     if(link=="tb"){
         linkToAdd = cTheboxLink;
-    }
-    if(link=="rps"){
-        linkToAdd = cRockpaperLink;
     }
     if(link=="somegame"){
         linkToAdd = cSomegameLink;
