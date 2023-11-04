a28641a363d6d44fecfdeb5c4f86ebd5e1704323
beaverbeg <beaverbeg@gmail.com>
2023-11-04 16:23:26 +0100

    aaa

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var d = data;
 var mydata = JSON.parse(d);
 
 var cLink =        mydata.main[0].link;
+
 var cFartbinLink =   mydata.fartbin[0].link;
 var cWebpassLink =   mydata.pass[0].link;
 var cCubeLink =      mydata.cube[0].link;
@@ -15,15 +16,16 @@ var cCubeDesc =      mydata.cube[1].desc;
 var cTheboxDesc =    mydata.tb[1].desc;
 var cSomegameDesc = mydata.somegame[1].desc;
 
-try{
-    document.getElementById("fartbin-desc").innerHTML = "- "+cFartbinDesc;
-    document.getElementById("pass-desc").innerHTML =    "- "+cWebpassDesc;
-    document.getElementById("cube-desc").innerHTML =    "- "+cCubeDesc;
-    document.getElementById("tb-desc").innerHTML =      "- "+cTheboxDesc;
-    document.getElementById("somegame-desc").innerHTML =     "- "+cSomegameDesc;
-}
-catch(err){
-    console.log("could not add description to projects\n");
+
+var desname = [cFartbinDesc, cWebpassDesc, cCubeDesc, cTheboxDesc, cSomegameDesc]
+var desid = ['fartbin-desc', 'pass-desc', 'cube-desc', 'tb-desc', 'somegame-desc']
+for(var i = 0; i<desid.length;i++){
+    try{
+        document.getElementById(desid[i]).innerHTML = "- "+desname[i];
+    }
+    catch(err){
+        console.log("could not add description to project");
+    }
 }
 
 
